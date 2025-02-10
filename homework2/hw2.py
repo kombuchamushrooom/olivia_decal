3fbd869c2e63d6ef7f8db98d60ac40d00a39ab90
Olivia Silva <olivia_lsilva@berkeley.edu>
2025-02-09 22:14:56 -0800

    Adding homework 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,20 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    ## To navigate directly to ~/Desktop, you would need to use the command "cd ~" 
 # 2) What does ~/ mean?
+    ## Indicates what parent directory you are currently in
 # 3) What's the difference between and abolute and a relative path in your terminal?
+    ## An absolute path tells you all of the "directions" it takes to get to a particular directory. A relative path only depends on the directories that are local in a way (i.e. how you can move up or down a directory relative to your current location)
 # 4) What command would bring you back to your home directory?
+    ## "cd ~"
 # 5) If you called rm ./ in your current working directory, what would it do? 
+    ## It would remove the current working directory
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    ## Git add would add all files/child directories to git hub. Git commit saves changes to your local repository and git push uploads your code to a remomte repository 
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    ## It would tell you what branch you are currently on and if there is anything to commit
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    ## I don't think I've been doing this long enough to run into bugs/errors, but I still struggle with remembering the syntax :/
 # 9) How does cloning a repository differ from pulling from a repository?
+    ## Cloning a repository will download it whereas pulling from a repository updates the code from said repository
 # 10) Tell me a fun fact!
+    ## The whole "I think therefore I am" statement made by Descartes seems like some abstract wishy-washy puzzle that is only referenced for pedantic reasons, but it's actually a self-verifying axiom. If there is one thing you can be certain of, it's that you're thinking, so you must at least exist as a thinking thing. Otherwise, you wouldn't be able to doubt that you didn't exist as a thinking thing. This alone doesn't suggest that you're a thinking thing with a body, but nonetheless serves as the archimedean point for other real things.
